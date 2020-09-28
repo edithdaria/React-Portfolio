d103fefdac083551cb9b855b11505e4e3bef180c
Tiger <63274912+edithdaria@users.noreply.github.com>
2020-09-27 20:09:31 -0400

    Updates

---
+++ precache-manifest.29827df716020cb1c0c638b45c0010c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb5e00adff8250e141c93bc139c1963",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "bc8e1cad3285e4426688",
+    "url": "/React-Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ea32e9dc8509da325faa",
+    "url": "/React-Portfolio/static/js/2.bcfaa5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.bcfaa5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc8e1cad3285e4426688",
+    "url": "/React-Portfolio/static/js/main.b64d6f49.chunk.js"
+  },
+  {
+    "revision": "cb7a9505cf195efa1427",
+    "url": "/React-Portfolio/static/js/runtime-main.da5ae62e.js"
+  }
+]);
